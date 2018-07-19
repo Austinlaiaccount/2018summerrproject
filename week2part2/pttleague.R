3e32cafff69067a57db4811d3a748c08a82d8304
Flake <austinlihu@gmail.com>
2018-07-19 22:05:14 +0800

    week2part2

---
+++ week2part2/pttleague.R
@@ -0,0 +1,10 @@
+
+source('pttTestFunction.R')
+#https://www.ptt.cc/bbs/LoL/index.html
+id = c(10000 : 10020)
+URL = paste0("https://www.ptt.cc/bbs/LoL/index", id, ".html")
+filename = paste0(id, ".txt")
+pttTestFunction(URL[1], filename[1])
+mapply(pttTestFunction, 
+       URL = URL, filename = filename)
+
